92f197a6caa97d5072fc65b5e5627e5fc4e51bb2
Van Phuong Thanh <nikoookami@hotmail.sg>
2015-05-29 17:27:01 +0800

    Some work done

--- SWEN/SWEN/AddStaff.Designer.cs
+++ SWEN/SWEN/AddStaff.Designer.cs
@@ -34,7 +34,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxBankAcc = new System.Windows.Forms.TextBox();
             this.textBoxPhone = new System.Windows.Forms.TextBox();
-            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPostalCode = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.comboBoxStaffLevel = new System.Windows.Forms.ComboBox();
@@ -43,11 +43,17 @@
             this.comboBoxDutyType = new System.Windows.Forms.ComboBox();
             this.dateTimePickerDoB = new System.Windows.Forms.DateTimePicker();
             this.Btn_AddStaff = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxStaffName
             // 
-            this.textBoxStaffName.Location = new System.Drawing.Point(105, 6);
+            this.textBoxStaffName.Location = new System.Drawing.Point(105, 64);
             this.textBoxStaffName.Name = "textBoxStaffName";
             this.textBoxStaffName.Size = new System.Drawing.Size(167, 20);
             this.textBoxStaffName.TabIndex = 0;
@@ -56,7 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 13);
             this.label1.TabIndex = 1;
@@ -65,16 +71,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Location = new System.Drawing.Point(12, 145);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Home address";
+            this.label2.Text = "Postal Code";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Location = new System.Drawing.Point(12, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 3;
@@ -82,7 +88,7 @@
             // 
             // textBoxBankAcc
             // 
-            this.textBoxBankAcc.Location = new System.Drawing.Point(105, 110);
+            this.textBoxBankAcc.Location = new System.Drawing.Point(105, 194);
             this.textBoxBankAcc.Name = "textBoxBankAcc";
             this.textBoxBankAcc.Size = new System.Drawing.Size(167, 20);
             this.textBoxBankAcc.TabIndex = 4;
@@ -90,24 +96,24 @@
             // 
             // textBoxPhone
             // 
-            this.textBoxPhone.Location = new System.Drawing.Point(105, 84);
+            this.textBoxPhone.Location = new System.Drawing.Point(105, 168);
             this.textBoxPhone.Name = "textBoxPhone";
             this.textBoxPhone.Size = new System.Drawing.Size(167, 20);
             this.textBoxPhone.TabIndex = 5;
             this.textBoxPhone.Text = "81282389";
             // 
-            // textBoxAddress
+            // textBoxPostalCode
             // 
-            this.textBoxAddress.Location = new System.Drawing.Point(105, 58);
-            this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(167, 20);
-            this.textBoxAddress.TabIndex = 6;
-            this.textBoxAddress.Text = "32 Geylang Lor 123";
+            this.textBoxPostalCode.Location = new System.Drawing.Point(105, 142);
+            this.textBoxPostalCode.Name = "textBoxPostalCode";
+            this.textBoxPostalCode.Size = new System.Drawing.Size(167, 20);
+            this.textBoxPostalCode.TabIndex = 6;
+            this.textBoxPostalCode.Text = "570023";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 113);
+            this.label4.Location = new System.Drawing.Point(12, 197);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 13);
             this.label4.TabIndex = 8;
@@ -116,7 +122,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 87);
+            this.label5.Location = new System.Drawing.Point(12, 171);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(76, 13);
             this.label5.TabIndex = 9;
@@ -129,7 +135,7 @@
             "Administration",
             "Management",
             "Receptionist"});
-            this.comboBoxStaffLevel.Location = new System.Drawing.Point(105, 136);
+            this.comboBoxStaffLevel.Location = new System.Drawing.Point(105, 220);
             this.comboBoxStaffLevel.Name = "comboBoxStaffLevel";
             this.comboBoxStaffLevel.Size = new System.Drawing.Size(167, 21);
             this.comboBoxStaffLevel.TabIndex = 10;
@@ -138,7 +144,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 139);
+            this.label6.Location = new System.Drawing.Point(12, 223);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(54, 13);
             this.label6.TabIndex = 11;
@@ -147,7 +153,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 166);
+            this.label7.Location = new System.Drawing.Point(12, 250);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(52, 13);
             this.label7.TabIndex = 13;
@@ -161,7 +167,7 @@
             "Room maintenance",
             "Estate maintenance",
             "Security"});
-            this.comboBoxDutyType.Location = new System.Drawing.Point(105, 163);
+            this.comboBoxDutyType.Location = new System.Drawing.Point(105, 247);
             this.comboBoxDutyType.Name = "comboBoxDutyType";
             this.comboBoxDutyType.Size = new System.Drawing.Size(167, 21);
             this.comboBoxDutyType.TabIndex = 12;
@@ -170,7 +176,7 @@
             // dateTimePickerDoB
             // 
             this.dateTimePickerDoB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerDoB.Location = new System.Drawing.Point(105, 32);
+            this.dateTimePickerDoB.Location = new System.Drawing.Point(105, 90);
             this.dateTimePickerDoB.Name = "dateTimePickerDoB";
             this.dateTimePickerDoB.Size = new System.Drawing.Size(167, 20);
             this.dateTimePickerDoB.TabIndex = 38;
@@ -178,7 +184,7 @@
             // 
             // Btn_AddStaff
             // 
-            this.Btn_AddStaff.Location = new System.Drawing.Point(197, 194);
+            this.Btn_AddStaff.Location = new System.Drawing.Point(197, 274);
             this.Btn_AddStaff.Name = "Btn_AddStaff";
             this.Btn_AddStaff.Size = new System.Drawing.Size(75, 23);
             this.Btn_AddStaff.TabIndex = 39;
@@ -186,11 +192,68 @@
             this.Btn_AddStaff.UseVisualStyleBackColor = true;
             this.Btn_AddStaff.Click += new System.EventHandler(this.btnAddStaff_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Username";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(105, 12);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(167, 20);
+            this.textBoxUserName.TabIndex = 40;
+            this.textBoxUserName.Text = "johnchan24";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 43;
+            this.label9.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(105, 38);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(167, 20);
+            this.textBoxPassword.TabIndex = 42;
+            this.textBoxPassword.Text = "*****";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(105, 116);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(167, 20);
+            this.textBoxAddress.TabIndex = 45;
+            this.textBoxAddress.Text = "32 Geylang Lor 123";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 119);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.TabIndex = 44;
+            this.label10.Text = "Home address";
+            // 
             // AddStaff
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 229);
+            this.ClientSize = new System.Drawing.Size(284, 309);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxUserName);
             this.Controls.Add(this.Btn_AddStaff);
             this.Controls.Add(this.dateTimePickerDoB);
             this.Controls.Add(this.label7);
@@ -199,7 +262,7 @@
             this.Controls.Add(this.comboBoxStaffLevel);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxPostalCode);
             this.Controls.Add(this.textBoxPhone);
             this.Controls.Add(this.textBoxBankAcc);
             this.Controls.Add(this.label3);
@@ -222,7 +285,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxBankAcc;
         private System.Windows.Forms.TextBox textBoxPhone;
-        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxPostalCode;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBoxStaffLevel;
@@ -231,5 +294,11 @@
         private System.Windows.Forms.ComboBox comboBoxDutyType;
         private System.Windows.Forms.DateTimePicker dateTimePickerDoB;
         private System.Windows.Forms.Button Btn_AddStaff;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label10;
     }
 }
--- SWEN/SWEN/AddStaff.cs
+++ SWEN/SWEN/AddStaff.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace Login
 {
     public partial class AddStaff : Form
     {
+
+        public static string connectionString = "Server=SIRIUS;Database=swen;User ID=bspiSCM;password=Nikoookami02";
+        public static SqlConnection cnn = new SqlConnection(connectionString);
+
         public AddStaff()
         {
             InitializeComponent();
@@ -24,14 +29,59 @@ namespace Login
 
         private void btnAddStaff_Click(object sender, EventArgs e)
         {
+
             // Get data from fields
+            string username= textBoxUserName.Text;
+            string password= textBoxPassword.Text;
             string staffName = textBoxStaffName.Text;
             DateTime dob = dateTimePickerDoB.Value;
-            string address = textBoxAddress.Text;
+            string address = textBoxPostalCode.Text;
+            string postalCode = textBoxPostalCode.Text;
             string phoneNum = textBoxPhone.Text;
             string bankAcc = textBoxBankAcc.Text;
             string staffLvl = comboBoxStaffLevel.Text;
             string dutyType = comboBoxDutyType.Text;
+            try
+            {
+                cnn.Open();
+
+                //bool success = doSomeWork(username, password, staffName, dob, address, postalCode,phoneNum, bankAcc, staffLvl, dutyType);
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        private bool doSomeWork(string username, string password, string staffName, DateTime dob, string address, string postalCode, string phoneNum, string bankAcc, string staffLvl, string dutyType)
+        {
+            string cmdString = "INSERT INTO Staff (staffUsername,staffPassword,staffName,staffBirthDate," +
+                "staffAddress,staffPostalCode,staffPhoneNo,staffBankAccNo,dutyId)" +
+            " VALUES (@valSatffUsername, @valStaffPassword,@valStaffName,@valStaffBirthDate,@valStaffAddress,@valStaffPostalCode,@valStaffPhoneNo," +
+            "@valStaffBankAccNo,@valDutyId)";
+
+            bool successOrNot = false;
+            using (SqlCommand comm = new SqlCommand())
+            {
+                comm.Connection = cnn;
+                comm.CommandText = cmdString;
+                comm.Parameters.AddWithValue("@valSatffUsername", username);
+                comm.Parameters.AddWithValue("@valStaffPassword", password);
+                comm.Parameters.AddWithValue("@valStaffName", staffName);
+                comm.Parameters.AddWithValue("@valStaffBirthDate", dob);
+                comm.Parameters.AddWithValue("@valStaffAddress", address);
+                comm.Parameters.AddWithValue("@valStaffPostalCode", postalCode);
+                comm.Parameters.AddWithValue("@valStaffPhoneNo", phoneNum);
+                comm.Parameters.AddWithValue("@valStaffBankAccNo", bankAcc);
+                comm.Parameters.AddWithValue("@valDutyId", dutyType);
+
+                comm.ExecuteNonQuery();
+                // Check Success
+                successOrNot = true;
+            }
+            return true;
         }
     }
 }
