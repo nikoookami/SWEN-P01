829740e8fe3564be2ad46c3c8b25d17a4913ad02
chunfoong <chunfoong95@hotmail.com>
2015-06-03 11:04:07 +0800

    Create operator

---
+++ SWEN/SWEN/class/DataBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    class DataBase
+    {
+        private const string GSM_CONN_STR = "data source=.\\SQLEXPRESS;initial catalog=DelonixRegia;integrated security=true;";
+
+        public static bool InsertProductID(booking u)
+        {
+            bool result = false;
+            SqlConnection conn = new SqlConnection();
+            SqlCommand comm = new SqlCommand();
+            if (u != null)
+            {
+
+                try
+                {
+                    conn = new SqlConnection();
+                    conn.ConnectionString = GSM_CONN_STR;
+                    conn.Open();
+                    comm.Connection = conn;
+                    comm.CommandText = "insert into Booking " +
+                            "(check_in_date, check_out_date,no_of_rooms,no_of_adults,no_of_children) values " +
+                            "(@check_in_date,@check_out_date,@no_of_rooms,@no_of_adults,@no_of_children);" 
+                            ;
+                    comm.Parameters.AddWithValue("(@check_in_date", u.Checkin);
+                    comm.Parameters.AddWithValue("@check_out_date", u.Checkout);
+                    comm.Parameters.AddWithValue("@no_of_rooms", u.NoOfRoom);
+                    comm.Parameters.AddWithValue("@no_of_adults", u.NoOfAdult);
+                    comm.Parameters.AddWithValue("@no_of_children", u.NoOfChildren);
+                    
+
+                    int rows = comm.ExecuteNonQuery();
+                    if (rows > 0)
+                        result = true;
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ SWEN/SWEN/class/booking.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class booking
+    {
+        private string checkin;
+
+        public string Checkin
+        {
+            get { return checkin; }
+            set { checkin = value; }
+        }
+        private string checkout;
+
+        public string Checkout
+        {
+            get { return checkout; }
+            set { checkout = value; }
+        }
+        private int noOfRoom;
+
+        public int NoOfRoom
+        {
+            get { return noOfRoom; }
+            set { noOfRoom = value; }
+        }
+        private int noOfAdult;
+
+        public int NoOfAdult
+        {
+            get { return noOfAdult; }
+            set { noOfAdult = value; }
+        }
+        private int noOfChildren;
+
+        public int NoOfChildren
+        {
+            get { return noOfChildren; }
+            set { noOfChildren = value; }
+        }
+
+        public booking(string checkin, string checkout, int noofroom, int noofadult, int noofchildren  )
+        {
+            this.checkin = checkin;
+            this.checkout = checkout;
+            this.noOfRoom = noofroom;
+            this.noOfAdult = noofadult;
+            this.noOfChildren = noofchildren;
+        }
+    }
+}
