5719805236bb212c4a279b9c849388675cb0e8a3
Van Phuong Thanh <nikoookami@hotmail.sg>
2015-06-04 13:42:57 +0800

    Login to SWEN

--- SWEN/SWEN/Reports/OverviewReport.cs
+++ SWEN/SWEN/Reports/OverviewReport.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using HTMLReport;
 using System.IO;
-using Login.Classes;
+using SWEN.Classes;
 
 namespace SWEN_Assignment_3.Reports
 {
--- SWEN/SWEN/Reservation.cs
+++ SWEN/SWEN/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SWEN;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
