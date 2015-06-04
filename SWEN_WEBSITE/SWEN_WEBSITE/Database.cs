e5978e285f583c014a9d5e08254bf102ff0c7e2f
chunfoong <chunfoong95@hotmail.com>
2015-06-04 12:53:27 +0800

    changes

--- SWEN_WEBSITE/SWEN_WEBSITE/Database.cs
+++ SWEN_WEBSITE/SWEN_WEBSITE/Database.cs
@@ -11,7 +11,7 @@ namespace SWEN_WEBSITE
 {
     public class Database
     {
-       private const string GSM_CONN_STR = "data source=.\\SQLEXPRESS;initial catalog=DelonixRegiaWebsite;integrated security=true;";
+        private const string GSM_CONN_STR = "data source=.\\SQLEXPRESS;initial catalog=DelonixRegia;integrated security=true;";
 
 
         public static bool InsertProductID(CustomerInfo u)
@@ -28,24 +28,30 @@ namespace SWEN_WEBSITE
                     conn.ConnectionString = GSM_CONN_STR;
                     conn.Open();
                     comm.Connection = conn;
-                    comm.CommandText = "insert into BokingInfo " +
-                             "(FullName,PhoneNum,Email,Address,Postal,Country,NoOfAdults,NoOfChildren,CheckInDate,CheckOutDate,NoOfROom,RoomType,PaymentType,Remarks) values " +
-                             "(@FullName,@PhoneNum,@Email,@Address,@Postal,@Country,@NoOfAdults,@NoOfChildren,@CheckInDate,@CheckOutDate,@NoOfROom,@RoomType,@PaymentType,@Remarks);";
+                    comm.CommandText = "insert into Guest " +
+                             "(guestid,name,phone,email,address,postalcode,country,paymenttype) values " +
+                             "(@guestid,@name,@phone,@email,@address,@postalcode,@country,@paymenttype);"+
+                             "insert into Booking " +
+                             "(bookingid,check_in_date,check_out_date,no_of_rooms,no_of_adults,no_of_children,guestid,staffid,roomno) values " +
+                             "(@bookingid,@check_in_date,@check_out_date,@no_of_rooms,@no_of_adults,@no_of_children,@guestid,@staffid,@roomno);";
 
-                    comm.Parameters.AddWithValue("@FullName", u.Name);
-                    comm.Parameters.AddWithValue("@PhoneNum", u.PhoneNumber);
-                    comm.Parameters.AddWithValue("@Email", u.Email);
-                    comm.Parameters.AddWithValue("@Address", u.Address);
-                    comm.Parameters.AddWithValue("@Postal", u.Postal);
-                    comm.Parameters.AddWithValue("@Country", u.Country);
-                    comm.Parameters.AddWithValue("@NoOfAdults", u.Adult);
-                    comm.Parameters.AddWithValue("@NoOfChildren", u.Children);
-                    comm.Parameters.AddWithValue("@CheckInDate", u.Checkiin);
-                    comm.Parameters.AddWithValue("@CheckOutDate", u.Checkout);
-                    comm.Parameters.AddWithValue("@NoOfROom", u.Noofroom1);
-                    comm.Parameters.AddWithValue("@RoomType", u.Roomtype);
-                    comm.Parameters.AddWithValue("@PaymentType", u.PaymentType);
-                    comm.Parameters.AddWithValue("@Remarks", u.Remarks);
+                    comm.Parameters.AddWithValue("@guestid", "12");
+                    comm.Parameters.AddWithValue("@name", u.Name);
+                    comm.Parameters.AddWithValue("@phone", u.PhoneNumber);
+                    comm.Parameters.AddWithValue("@email", u.Email);
+                    comm.Parameters.AddWithValue("@address", u.Address);
+                    comm.Parameters.AddWithValue("@postalcode", u.Postal);
+                    comm.Parameters.AddWithValue("@country", u.Country);
+                    comm.Parameters.AddWithValue("@paymenttype", u.PaymentType);
+
+                    comm.Parameters.AddWithValue("@bookingid", "131");
+                    comm.Parameters.AddWithValue("@check_in_date", u.Checkiin);
+                    comm.Parameters.AddWithValue("@check_out_date", u.Checkout);
+                    comm.Parameters.AddWithValue("@no_of_rooms", u.Noofroom1);
+                    comm.Parameters.AddWithValue("@no_of_adults", u.Adult);
+                    comm.Parameters.AddWithValue("@no_of_children", u.Children);
+                    comm.Parameters.AddWithValue("@staffid", "1212");
+                    comm.Parameters.AddWithValue("@roomno", u.Noofroom1);
 
                     int rows = comm.ExecuteNonQuery();
                     if (rows > 0)
--- SWEN_WEBSITE/SWEN_WEBSITE/WebForm4.aspx.cs
+++ SWEN_WEBSITE/SWEN_WEBSITE/WebForm4.aspx.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Xml;
+using System.Timers;
+
 
 namespace SWEN_WEBSITE
 {
@@ -14,8 +29,13 @@ namespace SWEN_WEBSITE
 
         }
 
+        static string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+        static UriBuilder uri = new UriBuilder(codeBase);
+        static string path = Uri.UnescapeDataString(uri.Path);
+        static string assemblyPath = Path.GetDirectoryName(path);
         protected void b1_Click(object sender, EventArgs e)
         {
+            
             string aa = TextBox.Text;
             string a = TextBox1.Text;
             string b = TextBox2.Text;
@@ -31,8 +51,16 @@ namespace SWEN_WEBSITE
             string l = TextBox12.Text;
             string o = TextBox13.Text;
 
-            CustomerInfo p = new CustomerInfo(aa, a, b, c, d, y, f, g, h, i, j, k, l, o);
-            Database.InsertProductID(p);
-        }
+           
+
+
+
+        
+                CustomerInfo p = new CustomerInfo(aa, a, b, c, d, y, f, g, h, i, j, k, l, o);
+                Database.InsertProductID(p);
+
+            }
+        
+
     }
 }
